1b27817204791d328d419c81adde286aff0fb0f1
Graham Fraser <fomhoire69@gmail.com>
2019-05-27 22:05:40 +0100

    First release

---
+++ C3SS_Web_Main/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace C3SS_Web_Main.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
